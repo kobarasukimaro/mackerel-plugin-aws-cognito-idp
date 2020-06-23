9add02b16bca9621492194b1d4d52c7cf2d190b1
kobarasukimaro <ryosei2001jp@gmail.com>
2020-06-23 09:30:59 +0900

    Fix import path

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-aws-cognito-idp/lib"
+import "github.com/kobarasukimaro/mackerel-plugin-aws-cognito-idp/lib"
 
 func main() {
 	mpawscognitoidp.Do()
